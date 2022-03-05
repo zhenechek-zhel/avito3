f59676bf39343f5d0b7f79b254eca781abe9d3b4
nurdas00 <nurdas00@gmail.com>
2022-03-05 21:43:46 +0300

    Address

--- src/main/java/com/amr/project/model/entity/Address.java
+++ src/main/java/com/amr/project/model/entity/Address.java
@@ -3,19 +3,34 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "address")
 @Data
 @Builder
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Long id;
+
     private String cityIndex;
+    @ManyToOne
+    @JoinColumn(name = "city_ID")
     private City city;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
     private String street;
     private String house;
+    @ManyToOne
+    @JoinTable(name = "user", joinColumns = @JoinColumn(name = "address_id"))
     private User user;
+
+    public Address() {
+
+    }
 }
