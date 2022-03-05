41e94c538003c2acc48fce8d088a89b85ad60181
kimart239 <kimartan11@gmail.com>
2022-03-06 03:13:54 +0900

    ym_shop

--- src/main/java/com/amr/project/model/entity/Shop.java
+++ src/main/java/com/amr/project/model/entity/Shop.java
@@ -1,70 +1,37 @@
 package com.amr.project.model.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "shop")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//@Builder
 public class Shop {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
     private String name;
-    @Column
     private String email;
-    @Column
     private String phone;
-    @Column
     private String description;
-
-    private int count;
-
-    private double rating;
-    // уточнить имя для связанной колонки
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "country_id")
+    @ManyToOne
+    @JoinColumn(name = "location_ID")
     private Country location;
-
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "shop")
+    @ManyToOne
     private List<Item> items;
-
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "shop")
+    @ManyToOne
     private List<Review> reviews;
-
-    @OneToOne(cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY)
-    @JoinColumn(name = "image_id")
+    @OneToOne
     private Image logo;
-
-    // уточнить имя для связанной колонки
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    private int count;
+    private double rating;
     private User user;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cartItem_id")
-    private CartItem cartItem;
-
-    @OneToMany(
-            mappedBy = "shop",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true
-    )
-    private Set<Feedback> feedback;
-
     private List<Discount> discounts;
-
     private boolean isModerated = false;
     private boolean isModerateAccept = false;
     private String moderatedRejectReason;
