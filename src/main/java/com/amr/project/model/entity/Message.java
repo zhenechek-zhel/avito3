36f38e19325f43777a4f842da76dd4ba90f2c8b8
Alexander1518 <haier85@mail.ru>
2022-03-06 09:47:10 +0300

    bezrukov-communications:chat,message,user

--- src/main/java/com/amr/project/model/entity/Message.java
+++ src/main/java/com/amr/project/model/entity/Message.java
@@ -3,15 +3,17 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "message")
 @Data
 @Builder
 public class Message {
+    @Id
+    @GeneratedValue
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
     private Chat chat;
     private User to;
     private User from;
