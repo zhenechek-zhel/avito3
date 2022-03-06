fd0cfaf2155ec8a2662ced57e04d30a2e514e0b0
Rozelux <rozelux@yandex.ru>
2022-03-06 16:40:14 +0300

    first commit

--- src/main/java/com/amr/project/model/entity/Message.java
+++ src/main/java/com/amr/project/model/entity/Message.java
@@ -3,16 +3,20 @@ package com.amr.project.model.entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "message")
 @Data
 @Builder
 public class Message {
+    @Id
     private Long id;
-    private Chat chat;
+    @ManyToMany
+
+    private Set<Chat> chats;
+
     private User to;
     private User from;
     private String textMessage;
