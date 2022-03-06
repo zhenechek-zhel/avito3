5e13270af22e3028bece450129b89c1a8c91fbc3
funre <magicisnear@bk.ru>
2022-03-06 17:21:24 +0300

    YuryM firstCommit City/Country mapping

--- src/main/java/com/amr/project/model/entity/Country.java
+++ src/main/java/com/amr/project/model/entity/Country.java
@@ -1,18 +1,26 @@
 package com.amr.project.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "country")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "country")
     private List<City> cities;
 }
