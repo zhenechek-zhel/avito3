4a315ba34b493f366cec37b88b137fc1df7e5b0b
Aslan <shangitbaev.aslan@gmail.com>
2022-03-11 16:10:49 +0300

    Swagger

--- src/main/java/com/amr/project/webapp/config/swagger/SwaggerConfig.java
+++ src/main/java/com/amr/project/webapp/config/swagger/SwaggerConfig.java
@@ -1,11 +1,46 @@
 package com.amr.project.webapp.config.swagger;
 
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //Заголовок страницы
+                .title("Spring Boot в сочетании с Swagger2 для создания RESTful API")
+                // основатель
+                .contact(new Contact("Aslan", "http://www.project.amr.com", "canfengli@126.com"))
+                //Описание
+                .description("Простой и элегантный стиль Restful")
+                .termsOfServiceUrl("http://blog.csdn.net/canfengli")
+                //номер версии
+                .version("1.0")
+                .build();
+    }
 }
