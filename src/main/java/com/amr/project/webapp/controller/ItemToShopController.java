799ff795a0c27c9b896151e7dbc0ed20ed8fdb19
funre <magicisnear@bk.ru>
2022-03-13 20:59:37 +0300

    ItemInShopController GetOne/GetAll

--- src/main/java/com/amr/project/webapp/controller/ItemToShopController.java
+++ src/main/java/com/amr/project/webapp/controller/ItemToShopController.java
@@ -1,12 +1,14 @@
 package com.amr.project.webapp.controller;
 
 import com.amr.project.converter.ItemMapper;
+import com.amr.project.converter.sets.ItemSetMapper;
 import com.amr.project.model.dto.ItemDTO;
 
 import com.amr.project.model.entity.Item;
 
 import com.amr.project.service.abstracts.ItemService;
 import com.amr.project.service.abstracts.ShopService;
+import com.paypal.api.payments.ItemList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,13 +33,25 @@ public class ItemToShopController {
         this.shopService = shopService;
     }
 
-    @GetMapping
+    @GetMapping("/shop/{idShop}/items")
+    public ResponseEntity<Set<ItemDTO>> getAllItemsInShop( @PathVariable(name = "idShop") Long idShop) {
+        return new ResponseEntity<Set<ItemDTO>>(ItemSetMapper.INSTANCE
+                .toDTOSet(shopService.getShopById(idShop).getItems()), HttpStatus.OK);
+    }
 
-    @PutMapping("/shop/{idShop}/items/{idItem}")
-    public ResponseEntity<Set<Item>> addItemInShop(
-            @PathVariable(name = "idItem") Long idItem,
-            @PathVariable(name = "idShop") Long idShop,
-            @RequestBody ItemDTO itemDtoToAdd) {
+    @GetMapping("/shop/{idShop}/items/{idItem}")
+        public ResponseEntity<ItemDTO> getItemInShop( @PathVariable(name = "idShop") Long idShop,
+                                                @PathVariable(name = "idItem") Long idItem) {
+        if (shopService.getShopById(idShop).getItems().contains(itemService.getItemById(idItem))) {
+            return new ResponseEntity<ItemDTO>(ItemMapper.INSTANCE
+                    .toDTO(itemService.getItemById(idItem)),HttpStatus.OK) ;
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    @PostMapping("/shop/{idShop}/items")
+    public ResponseEntity<Set<Item>> addItemInShop( @PathVariable(name = "idShop") Long idShop,
+                                                    @RequestBody ItemDTO itemDtoToAdd) {
         Item item = ;
         Set<Item> items = shopService.getShopById(idShop).getItems();
         if (!items.contains(item)) {
@@ -50,20 +64,18 @@ public class ItemToShopController {
         return new ResponseEntity<>(items, HttpStatus.OK);
     }
 
-
-
     @DeleteMapping("/shop/{idShop}/items/{idItem}")
-    public ResponseEntity<Set<Item>> deleteItemFromShop(
+    public ResponseEntity<HttpStatus> deleteItemFromShop(
             @PathVariable(name = "idItem") Long idItem,
             @PathVariable(name = "idShop") Long idShop,
             @RequestBody ItemDTO itemDtoToDelete) {
-
-        Item item = ItemMapper.INSTANCE.toEntity(itemDtoToDelete);
-        Set<Item> items = shopService.getShopById(idShop).getItems();
-
+        Item item = ItemMapper.INSTANCE.toEntity(itemDtoToDelete); // полученный JSON преобразуем в Entity
+        Set<Item> items = shopService.getShopById(idShop).getItems(); // вытаскиваем коллекцию всех предметов, которые находятся в магазине
         if (items.contains(item)) {
-            items.remove(itemService.getItemById(item.getId()));
+            items.remove(itemService.getItemById(idItem));
+            return new ResponseEntity<>(HttpStatus.OK);
         }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @PatchMapping("/shop/{idShop}/items/{idItem}")
@@ -83,7 +95,6 @@ public class ItemToShopController {
         items.stream()
                 .map(ItemMapper.INSTANCE::toItemDTO)
                 .collect(Collectors.toSet());
-
         return new ResponseEntity<>(items, HttpStatus.OK);
     }
 }
