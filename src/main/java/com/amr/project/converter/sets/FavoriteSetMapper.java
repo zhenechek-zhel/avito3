b6590460e823dd191150cfef7912682d52346dcc
Evgeniy <zhel186@mail.ru>
2022-03-13 14:42:33 +0300

    mappers in working

--- src/main/java/com/amr/project/converter/sets/FavoriteSetMapper.java
+++ src/main/java/com/amr/project/converter/sets/FavoriteSetMapper.java
@@ -2,9 +2,7 @@ package com.amr.project.converter.sets;
 
 import com.amr.project.converter.FavoriteMapper;
 import com.amr.project.model.dto.FavoriteDTO;
-import com.amr.project.model.dto.ReviewDTO;
 import com.amr.project.model.entity.Favorite;
-import com.amr.project.model.entity.Review;
 import org.mapstruct.Mapper;
 
 import java.util.Set;
