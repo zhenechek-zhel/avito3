4675f6a5f61bd188ca5f387c0732fb8e026993c4
funre <magicisnear@bk.ru>
2022-03-13 17:50:20 +0300

    LastMappersUpdate

--- src/main/java/com/amr/project/converter/UserInfoMapper.java
+++ src/main/java/com/amr/project/converter/UserInfoMapper.java
@@ -6,7 +6,7 @@ import com.amr.project.model.entity.UserInfo;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = UserMapper.class, GenderMapper.class )
+@Mapper(uses = {UserMapper.class, GenderMapper.class})
 public interface UserInfoMapper {
     UserInfoMapper INSTANCE = Mappers.getMapper(UserInfoMapper.class);
     UserInfoDTO toDTO(UserInfo userInfo);
